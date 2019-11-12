37c0589830e90c41b1655ad140ed4627394206fa
Maksim Kildishev <thanatormax@gmail.com>
2019-11-12 19:29:40 +0300

    File for loader deleted, loader function added

--- lab2/src/dataset_loader.py
+++
@@ -1,5 +0,0 @@
-# This file is supposed to have a dataset loaded
-
-
-def get_cifar_data():
-    pass
--- lab2/src/source.py
+++ lab2/src/source.py
@@ -4,3 +4,13 @@
 # PLEASE, PAY ATTENTION!
 # If you will create a folder for new lab, don't forget to include venv and .idea and another system
 # directories to .gitignore
+
+import glob
+from PIL import Image
+import numpy as np
+
+
+def get_cifar_data(path='C:\\Users\\Максим\\Desktop\\Test Data for lab2\\train\\train\\*.png', count=10):
+    filelist = glob.glob(path)[:count]
+    x = np.array([np.array(Image.open(fname)) for fname in filelist])
+    return x
