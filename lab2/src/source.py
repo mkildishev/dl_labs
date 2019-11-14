8f44c88581f3a41cfd7004d99781550577728493
Max <thanatormax@gmail.com>
2019-11-15 02:51:09 +0300

    Now data loading is carried out through keras. Argument parser added.

--- lab2/src/source.py
+++ lab2/src/source.py
@@ -5,12 +5,50 @@
 # If you will create a folder for new lab, don't forget to include venv and .idea and another system
 # directories to .gitignore
 
-import glob
-from PIL import Image
+from argparse import ArgumentParser
 import numpy as np
+from numpy import load
+from keras import models
+from keras.datasets import mnist
+from keras import layers
+from keras.utils import to_categorical
+from keras.datasets import cifar10
+from keras import optimizers
 
 
-def get_cifar_data(path='C:\\Users\\Максим\\Desktop\\Test Data for lab2\\train\\train\\*.png', count=10):
-    filelist = glob.glob(path)[:count]
-    x = np.array([np.array(Image.open(fname)) for fname in filelist])
-    return x
+def create_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--hidden_num', type=int,  default=50000,
+                        help='Hidden layer nodes num')
+    parser.add_argument('--batch_size', type=int, default=128,
+                        help='Sample batch size')
+    parser.add_argument('--rate', type=float, default=0.1,
+                        help='Learning rate')
+    parser.add_argument('--epochs', type=int, default=5,
+                        help='Epochs count')
+    return parser
+
+
+def preprocessing_data(x_train, y_train, x_test, y_test):
+    y_train_cat = to_categorical(y_train)
+    y_test_cat = to_categorical(y_test)
+    x_train_cat = x_train.astype('float32') / 255
+    x_test_cat = x_test.astype('float32') / 255
+    return (x_train_cat, y_train_cat), (x_test_cat, y_test_cat)
+
+
+def run(hidden_size=10000, batch_size=128, rate=0.1, epochs=20):
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = preprocessing_data(x_train, y_train, x_test, y_test)
+    # todo: use keras NN model here
+    return
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    main(parser.parse_args())
+
