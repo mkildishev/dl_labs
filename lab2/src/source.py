819551155f23b92745871c7b283e6861c12f7c1d
Max <thanatormax@gmail.com>
2019-11-21 02:00:25 +0300

    Best achieved result: loss: 0.6123 - accuracy: 0.7982 - val_loss: 1.4945 - val_accuracy: 0.5398

--- lab2/src/source.py
+++ lab2/src/source.py
@@ -6,10 +6,8 @@
 # directories to .gitignore
 
 from argparse import ArgumentParser
-import numpy as np
-from numpy import load
+from keras.layers import BatchNormalization
 from keras import models
-from keras.datasets import mnist
 from keras import layers
 from keras.utils import to_categorical
 from keras.datasets import cifar10
@@ -18,13 +16,13 @@ from keras import optimizers
 
 def create_parser():
     parser = ArgumentParser()
-    parser.add_argument('--hidden_num', type=int,  default=50000,
+    parser.add_argument('--hidden_num', type=int,  default=300,
                         help='Hidden layer nodes num')
     parser.add_argument('--batch_size', type=int, default=128,
                         help='Sample batch size')
     parser.add_argument('--rate', type=float, default=0.1,
                         help='Learning rate')
-    parser.add_argument('--epochs', type=int, default=5,
+    parser.add_argument('--epochs', type=int, default=20,
                         help='Epochs count')
     return parser
 
@@ -39,23 +37,19 @@ def preprocessing_data(x_train, y_train, x_test, y_test):
     return (x_train_cat, y_train_cat), (x_test_cat, y_test_cat)
 
 
-def run(hidden_size=300, batch_size=64, rate=0.001, epochs=120):
-    # todo: try data augmentation and adaptive learning rate and batch normalization
+def run(hidden_size=300, batch_size=128, rate=0.1, epochs=20):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     (x_train, y_train), (x_test, y_test) = preprocessing_data(x_train, y_train, x_test, y_test)
     network = models.Sequential()
-    network.add(layers.Dense(hidden_size, activation='relu', kernel_initializer='he_normal', input_shape=(32 * 32 * 3,)))
-    network.add(layers.Dense(10, activation='softmax'))
-    #optimizer = optimizers.Adam(lr=1e-2, decay=1e-2/epochs)
-    #optimizer = optimizers.RMSprop(rate)
-    optimizer = optimizers.SGD(lr=1e-2, momentum=0.9, decay=1e-2/epochs)
-    #optimizer = optimizers.Adagrad(lr=0.01, epsilon=1e-08, decay=0.0)
+    network.add(BatchNormalization(input_shape=(32 * 32 * 3,)))
+    network.add(layers.Dense(hidden_size, activation='relu', kernel_initializer='he_normal'))
+    network.add(layers.Dense(10,  activation='softmax'))
+    optimizer = optimizers.SGD(lr=rate, decay=1e-2/epochs)
     network.compile(optimizer=optimizer,
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
-    network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs, batch_size=batch_size,  verbose=2)
-
-    return
+    network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
+                batch_size=batch_size,  verbose=2)
 
 
 def main(args):
