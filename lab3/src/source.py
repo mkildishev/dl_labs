5a2144ae3d1893033f7de47953c5e2a6a8f118da
Max <thanatormax@gmail.com>
2019-11-22 21:37:37 +0300

    Initial commit for third lab

---
+++ lab3/src/source.py
@@ -0,0 +1,6 @@
+# A little introductory for you, folks.
+# We are writing a program to solve multi-classification problem
+# https://www.kaggle.com/c/cifar-10/overview dataset from here
+# PLEASE, PAY ATTENTION!
+# If you will create a folder for new lab, don't forget to include venv and .idea and another system
+# directories to .gitignore
