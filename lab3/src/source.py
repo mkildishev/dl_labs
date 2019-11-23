7db5944a2475099736b817ac6b62dfb2d60eed6b
Max <thanatormax@gmail.com>
2019-11-24 00:49:46 +0300

    Added simple model Test accuracy: ~0.65

--- lab3/src/source.py
+++ lab3/src/source.py
@@ -6,10 +6,13 @@
 # directories to .gitignore
 
 from argparse import ArgumentParser
-from keras import models
+from keras import models, Sequential
 from keras import layers
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import SGD
 from keras.utils import to_categorical
 from keras.datasets import cifar10
+import tensorflow as tf
 
 
 def create_parser():
@@ -50,15 +53,31 @@ def print_info(time, score_train, score_test):
     print('Train time: ', time)
 
 
+
 def load_and_run(path_to_model, path_to_weight):
     pass
 
 
 def fit_and_run(hidden_size=300, batch_size=128, rate=0.1, epochs=20):
-    pass
-
-
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = preprocessing_data(x_train, y_train, x_test, y_test)
+    network = Sequential()
+    network.add(Conv2D(32, (3, 3), activation='relu',
+                     kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+    network.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    network.add(MaxPooling2D((2, 2)))
+    network.add(Flatten())
+    network.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    network.add(Dense(10, activation='softmax'))
+    optimizer = SGD(lr=0.001, momentum=0.9)
+    network.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
+                batch_size=batch_size, verbose=2)
+    score_train = network.evaluate(x_train, y_train, verbose=0)
+    score_test = network.evaluate(x_test, y_test, verbose=0)
+    return score_train, score_test
 
 
 if __name__ == '__main__':
-    parser = create_parser()
+    score_train, score_test = fit_and_run()
+    print_info(0, score_train, score_test)
