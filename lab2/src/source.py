620d660c1d42b2425d4a3147d0ecdf2206bc7b7c
Max <thanatormax@gmail.com>
2019-12-16 04:06:56 +0300

    Added functions for graphics generations

--- lab2/src/source.py
+++ lab2/src/source.py
@@ -5,6 +5,8 @@
 # If you will create a folder for new lab, don't forget to include venv and .idea and another system
 # directories to .gitignore
 
+import keras
+import pydot as pyd
 from datetime import datetime
 from argparse import ArgumentParser
 from keras.engine.saving import model_from_json
@@ -14,13 +16,17 @@ from keras import layers
 from keras.utils import to_categorical
 from keras.datasets import cifar10
 from keras import optimizers
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from keras.utils import plot_model
+keras.utils.vis_utils.pydot = pyd
 
 
 def create_parser():
     parser = ArgumentParser()
     parser.add_argument('--mode', type=str, choices=['load', 'fit'], default='fit',
                         help='Choosing work mode')
-    parser.add_argument('--model', type=str,  default='3',
+    parser.add_argument('--model', type=str,  default='2',
                         help='Model type')
     parser.add_argument('--batch_size', type=int, default=128,
                         help='Sample batch size')
@@ -43,6 +49,7 @@ def second_model():
     network.add(BatchNormalization())
     network.add(layers.Dense(300, activation='relu', kernel_initializer='he_normal'))
     network.add(layers.Dense(10,  activation='softmax'))
+   # plot_model(network, rankdir='TB', to_file='test_keras_plot_model.png', show_shapes=True)
     return network
 
 
@@ -74,6 +81,24 @@ def save_network(network, name):
         json_file.write(model_json)
 
 
+def build_name_for_obj(format, *args):
+    name = '_'.join([*args])
+    name += format
+    return name
+
+
+def save_comparision_plot(train_values, validation_values, y_label, model_name, plot_name):
+    fig, ax = plt.subplots()
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    plt.plot(train_values, marker='o')
+    plt.plot(validation_values, marker='o')
+    plt.title(plot_name)
+    plt.ylabel(y_label)
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'])
+    plt.savefig(build_name_for_obj('.png', model_name, plot_name))
+
+
 def load_and_run(path_to_model, path_to_weight):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     (x_train, y_train), (x_test, y_test) = preprocessing_data(x_train, y_train, x_test, y_test)
@@ -99,8 +124,13 @@ def fit_and_run(model=first_model, name="model", batch_size=128, rate=0.1, epoch
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
     time_start = datetime.now()
-    network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
+    history = network.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs,
                 batch_size=batch_size,  verbose=2)
+
+    save_comparision_plot(history.history['accuracy'], history.history['val_accuracy'], 'Accuracy',
+                          name, 'Network accuracy')
+    save_comparision_plot(history.history['loss'], history.history['val_loss'], 'Loss',
+                          name, 'Network loss')
     save_network(network, name)
     delta_time = datetime.now() - time_start
     score_train = network.evaluate(x_train, y_train, verbose=0)
